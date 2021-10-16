65543b1346bfbd74f8a0cb9ac5df81d4082f3491
davidtheblane <davidtheblane@gmail.com>
2021-10-16 16:02:10 -0300

    update: message depends on database connected

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const DATABASE_URL = process.env.DATABASE_URL;
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const NODE_ENV = process.env.NODE_ENV
+
 
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
@@ -11,4 +13,10 @@ module.exports = mongoose;
 //verifica se o banco de dados esta conectado
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('📦 Connected to the database'))
+db.once('open', () => {
+  if (NODE_ENV === "development") {
+    console.log('📦 Connected to "development" database')
+  } else {
+    console.log('📦 Connected to the database')
+  }
+})
