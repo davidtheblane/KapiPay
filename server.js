7f1d8f809d8494c622d115419d952478b8b1af14
davidtheblane <davidtheblane@gmail.com>
2021-10-21 13:24:07 -0300

    update: included sentry, need arrangements

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require('morgan')
 const cors = require('cors');
 const multer = require('multer')
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
+const sentryConfig = require("./src/config/sentry")
 
 require('dotenv').config({
   path: process.env.NODE_ENV === "development" ? ".env.development" : ".env"
@@ -20,6 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+Sentry.init(sentryConfig);
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler())
+
+
+
+//ROUTES
 app.use('/', require('./src/routes/users.routes'))
 app.use('/account', require('./src/routes/account.routes'))
 
