4a423a50d0cd3beb18077669f160a65ab65a45b8
davidtheblane <davidtheblane@gmail.com>
2021-10-22 14:39:51 -0300

    update: cleaning cep controllers garbage

--- src/controllers/cep.controller.js
+++ src/controllers/cep.controller.js
@@ -8,12 +8,8 @@ module.exports = {
 
   getCep: async (req, res) => {
     try {
-      console.log('chegou no controller')
       const connection = await service.connection();
-      // console.log(connection);
-
-      const response = await service.request(connection, "consultaCEPAsync", { cep: req.params })
-
+      const response = await service.request(connection, "consultaCEPAsync", { cep: `${req.params.id}` })
       return res.status(200).send(response[0] || console.log(response[0]))
     } catch (err) {
       return res.status(err.status || 400).send({ message: err.stack });
@@ -21,12 +17,3 @@ module.exports = {
 
   }
 }
-
-// async function getCep(req, res) {
-//   const connection = await service.connection();
-//   // console.log(connection);
-//   const response = await service.request(connection, "consultaCEPAsync", { cep: "11050300" })
-//   return console.log(response[0]);
-//   res.send(response[0])
-// }
-// getCep()
