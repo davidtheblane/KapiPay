df63752799c12c9cf074401299a71db3f7b34f6c
davidtheblane <davidtheblane@gmail.com>
2021-10-27 14:38:28 -0300

    fix: serving cep via SOAP

--- src/resources/lib/cep/cep.service.js
+++ src/resources/lib/cep/cep.service.js
@@ -1,18 +1,29 @@
-const soap = require('soap');
+const axios = require('axios');
+const xmlToJson = require('xml-to-json-stream');
+const parser = xmlToJson({ attributeMode: false });
 
-class Soap {
-  constructor(url) {
-    this.url = url;
-  }
-
-  async connection() {
-    const url = this.url;
-    return soap.createClientAsync(url)
-  }
-
-  async request(connection, method, body) {
-    return await connection[method](body)
+const getCep = {
+  async postCode(obj) {
+    const url = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl';
+    const req = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+                    <soapenv:Header />
+                      <soapenv:Body>
+                        <consultaCEP xmlns="http://cliente.bean.master.sigep.bsb.correios.com.br/">
+                          <cep xmlns="">${obj}</cep>
+                        </consultaCEP>
+                      </soapenv:Body>
+                    </soapenv:Envelope>`;
+    const { data } = await axios.post(url, req, {
+      headers: {
+        "Content-type": "text/xml;charset=utf-8",
+      }
+    })
+    const res = parser.xmlToJson(data, (err, json) => {
+      if (err) return err
+      return json
+    })
+    return res['soap:Envelope']['soap:Body']['ns2:consultaCEPResponse'].return;
   }
 }
 
-module.exports = Soap;
+module.exports = getCep
